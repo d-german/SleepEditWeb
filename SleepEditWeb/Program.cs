7e25e98f6d5d2703a7ff9418765c13b9a47653d2
Damon German <damon.german@hyland.com>
2025-12-18 20:26:43 -0600

    Revamp layout and styling for Medication List and main application, adding dark theme support and enhancing user interaction elements.

--- SleepEditWeb/Program.cs
+++ SleepEditWeb/Program.cs
@@ -40,7 +40,7 @@ public class Program
 
 		app.MapControllerRoute(
 			name: "default",
-			pattern: "{controller=Home}/{action=Index}/{id?}");
+			pattern: "{controller=MedList}/{action=Index}/{id?}");
 
 		app.Run();
 	}
